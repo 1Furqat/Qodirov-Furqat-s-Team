742991611a72cbc494754767fd5ab1c6b7b2ffb1
Qodirov Furqat <qodirovfurqatbek1413@gmail.com>
2022-02-07 06:28:19 -0800

    Webview app

---
+++ app/src/main/java/com/example/simple/MainActivity.java
@@ -0,0 +1,54 @@
+package com.example.simple;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.os.Bundle;
+import android.view.KeyEvent;
+import android.view.View;
+import android.webkit.ClientCertRequest;
+import android.webkit.WebSettings;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+import android.widget.Button;
+import android.widget.ProgressBar;
+
+public class MainActivity extends AppCompatActivity {
+WebView webView;
+ProgressBar progressBar;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        webView=(WebView) findViewById(R.id.webview1);
+        progressBar=findViewById(R.id.progressbar);
+        webView.loadUrl("https://kundalik.com");
+        WebSettings webSettings= webView.getSettings();
+        webSettings.setJavaScriptEnabled(true);
+        WebViewClient webViewClient=new WebViewClient(){
+            @Override
+            public void onPageStarted(WebView view, String url, Bitmap favicon) {
+                progressBar.setVisibility(View.VISIBLE);
+            }
+
+            @Override
+            public void onPageFinished(WebView view, String url) {
+                progressBar.setVisibility(View.GONE);
+            }
+        };
+        webView.setWebViewClient(webViewClient);
+        webSettings.setBuiltInZoomControls(true);
+    }
+    @Override
+    public boolean onKeyDown(int keyCode, KeyEvent event) {
+        // Check if the key event was the Back button and if there's history
+        if ((keyCode == KeyEvent.KEYCODE_BACK) && webView.canGoBack()) {
+            webView.goBack();
+            return true;
+        }
+        // If it wasn't the Back key or there's no web page history, bubble up to the default
+        // system behavior (probably exit the activity)
+        return super.onKeyDown(keyCode, event);
+    }
+}
---
+++ app/src/main/java/com/example/simple/MainActivity2.java
@@ -0,0 +1,24 @@
+package com.example.simple;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity2 extends AppCompatActivity {
+Button button1;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main2);
+        button1=findViewById(R.id.button2);
+        button1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(MainActivity2.this,MainActivity.class));
+            }
+        });
+    }
+}
